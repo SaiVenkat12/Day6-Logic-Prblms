7973783143f0770dfec799f08d8821a13ebbfae3
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-02 22:24:49 +0530

    [SaiVenkat]-StopWatch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StopWatch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number to start the Stopwatch: ");
+            int Start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter any number toend the Stopwatch: ");
+            int Stop = Convert.ToInt32(Console.ReadLine());
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+            Console.WriteLine("Time Elapsed : {0}",
+            stopwatch.Elapsed);
+
+        }
+
+    }
+}
