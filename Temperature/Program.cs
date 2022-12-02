c31ced7057c3df1edcb48d4f8e4f740b24135a64
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-02 16:44:42 +0530

    [SaiVenkat]-TemperatureConversion

---
+++ Temperature/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Temperature
+{
+    internal class program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Temperature Conversion");
+            Console.WriteLine("To convert Celsius to Faherheit Select : 1");
+            Console.WriteLine("To convert Faherheit to Celsius Select : 2");
+
+                 int choice = Convert.ToInt16(Console.ReadLine());
+            double celsius;
+            double fahrenheit;
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the Temperature in Celsius");
+                    celsius = double.Parse(Console.ReadLine());
+                    
+                    fahrenheit = (celsius * 9) / 5 + 32;
+                    Console.WriteLine(celsius + " Celsius to Fahrernheit : " + fahrenheit);
+                    
+                    break;
+
+                    case 2:
+                    Console.WriteLine("Enter the Temperature in Fahrenheit :");
+                    fahrenheit = double.Parse(Console.ReadLine());
+
+                    celsius = (fahrenheit - 32) * 5 / 9;
+                    Console.WriteLine(fahrenheit + " faherhheit to Celsius: " + celsius);
+
+                    break;
+
+                    default:
+                    Console.WriteLine("Enter the correct option");
+                    break;
+
+             
+            }
+        }
+    }
+}
