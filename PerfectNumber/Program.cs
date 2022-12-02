6c980e205a8ddb911e14f9cf74f76f2718534b2b
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-02 13:34:26 +0530

    [SaiVenkat]-Perfect Number

---
+++ PerfectNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace PerfectNumber
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to check : ");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i < n; i++)
+            { 
+                if( n % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (n == sum)
+            {
+                Console.WriteLine(n + " is a Perfect Number.");
+            }
+            else
+            {
+                Console.WriteLine(n + " is Not a Perfect Number.");
+            }
+
+        }
+    }
+}
