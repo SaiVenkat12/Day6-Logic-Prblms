68d683b24f3c24dce92d62d3264bf92bcab232bc
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-02 23:06:38 +0530

    [SaiVenkat]-Square root of a Number

---
+++ Sqrt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Sqrt
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the Non Negitive Integer to find the Square Root :");
+            int c = int.Parse(Console.ReadLine());
+            if (c > 0 ) 
+            {
+                double t = c;
+                double epsilon = 1e-15;
+                while (Math.Abs(t - c / t) > epsilon*t)
+                {
+                    t = (c / t + t) / 2;
+                }
+                Console.WriteLine("Square Root of {0} is {1}", c, t);
+            }
+            else
+            {
+                Console.WriteLine("The Number entered {0} is not applicable.", c);
+                Console.WriteLine("Enter only Non negitive integers");
+            }
+        }
+    }
+}
--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -9,13 +9,14 @@ namespace StopWatch
         {
             Console.WriteLine("Enter any number to start the Stopwatch: ");
             int Start = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Enter any number toend the Stopwatch: ");
             int Stop = Convert.ToInt32(Console.ReadLine());
+
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
             stopwatch.Stop();
-            Console.WriteLine("Time Elapsed : {0}",
-            stopwatch.Elapsed);
+            Console.WriteLine("Time Elapsed : {0}",stopwatch.Elapsed);
 
         }
 
