4421de3d7d051f171295156e3787158043468fc3
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-02 11:52:38 +0530

    [SaiVenkat]-Fibnonacci Series

---
+++ Fibnonacci/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Fibnonacci
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the of number : ");
+            int n = int.Parse(Console.ReadLine());
+            int a=0; 
+            int b=1;
+            int sum=0;
+            if (n==0)
+            {
+                Console.WriteLine("The Value of Fibonancci Series = 0");
+            }
+            else if (n==1)
+            {
+                Console.WriteLine("The Value of Fibonancci Series = 1");
+            }
+            else if (n > 0)
+            {
+                for (int i = 2; i <= n; i++)
+                {
+                    sum = a + b;
+                    a = b;
+                    b = sum;
+                }
+                Console.WriteLine("The Value of Fibonancci Series = " + sum);
+            }
+
+            else
+            {
+                Console.WriteLine("Enter the approriate value. ");
+            }
+                      
+
+
+        }
+    }
+}
