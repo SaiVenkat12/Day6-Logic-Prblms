24b576ab93dd94b16356a502c32e0932c30fa0b3
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-02 13:46:58 +0530

    [SaiVenkat]-Reverse a Number

---
+++ Reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Reverse
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to Reverse : ");
+            int n = int.Parse(Console.ReadLine());
+            int reverse = 0;
+            int rem;
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+            }
+            Console.Write("Reversed Number: " + reverse);
+
+        }
+    }
+}
