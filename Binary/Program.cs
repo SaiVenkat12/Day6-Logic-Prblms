5f47b13fcc799774361bfa2697802e4d86a3eb81
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-03 12:29:34 +0530

    [SaiVenkat]-Convert to Binary

---
+++ Binary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Binary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Number : ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            string bi = Convert.ToString((byte)n, 2);
+            Console.WriteLine(n + " in binary = " + bi);
+        }
+    }
+}
